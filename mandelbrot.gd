ed98789ee66c6ae0e18ac63ad0da45ccb5c1e030
Ash <148392742+ashilated@users.noreply.github.com>
2026-04-25 15:58:59 -0700

    .

--- mandelbrot.gd
+++ mandelbrot.gd
@@ -1,28 +1,85 @@
 extends Node2D
 
-var time: float = 0
-var power: float = 0.1
-var current_pos: Vector2 = Vector2(0, 0)
+var zoom: float = 4
+var dragging: bool = false
 var last_mouse_pos: Vector2 = Vector2(0, 0)
-@onready var zoom: HSlider = $VBoxContainer/Zoom
+var offset: Vector2 = Vector2(0, 0)
+@onready var color1 = Vector4($VBoxContainer/Color1/R.value, $VBoxContainer/Color1/G.value, $VBoxContainer/Color1/B.value, 1)
+@onready var color2 = Vector4($VBoxContainer/Color2/R.value, $VBoxContainer/Color2/G.value, $VBoxContainer/Color2/B.value, 1)
+@onready var accent = Vector4($VBoxContainer/Accent/R.value, $VBoxContainer/Accent/G.value, $VBoxContainer/Accent/B.value, 1)
 @onready var shader: ShaderMaterial = $ColorRect.material
 
+func _ready() -> void:
+	shader.set_shader_parameter("color1", color1)
+	shader.set_shader_parameter("color2", color2)
+	shader.set_shader_parameter("accent", accent)
+
 func _process(delta: float) -> void:
-	#time += delta
-	#power += delta * 0.1
-	#var zoom: float = 6 / pow(time, power)
-	var zoom: float = zoom.value
+	if Input.is_action_just_released("scroll_up"):
+		zoom *= 0.9
+	elif Input.is_action_just_released("scroll_down"):
+		zoom /= 0.9
 	shader.set_shader_parameter("zoom", zoom)
+
+func _on_color_rect_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			dragging = event.pressed
+			last_mouse_pos = event.position
+
+	if event is InputEventMouseMotion and dragging:
+		var delta = event.position - last_mouse_pos
+		last_mouse_pos = event.position
+
+		offset -= delta * 0.01 * zoom
+		shader.set_shader_parameter("offset", offset)
+
+
+func _on_iterations_value_changed(value: float) -> void:
+	shader.set_shader_parameter("max_iterations", value)
+
+func _on_power_value_changed(value: float) -> void:
+	shader.set_shader_parameter("power", value)
+
+
+func _on_color1_r_value_changed(value: float) -> void:
+	color1.x = value
+	shader.set_shader_parameter("color1", color1)
+
+func _on_color1_g_value_changed(value: float) -> void:
+	color1.y = value
+	shader.set_shader_parameter("color1", color1)
+
+func _on_color1_b_value_changed(value: float) -> void:
+	color1.z = value
+	shader.set_shader_parameter("color1", color1)
+
+
+func _on_color2_r_value_changed(value: float) -> void:
+	color2.x = value
+	shader.set_shader_parameter("color2", color2)
+
+func _on_color2_g_value_changed(value: float) -> void:
+	color2.y = value
+	shader.set_shader_parameter("color2", color2)
+
+func _on_color2_b_value_changed(value: float) -> void:
+	color2.z = value
+	shader.set_shader_parameter("color2", color2)
+
+
+func _on_accent_r_value_changed(value: float) -> void:
+	accent.x = value
+	shader.set_shader_parameter("accent", accent)
+
+func _on_accent_g_value_changed(value: float) -> void:
+	accent.y = value
+	shader.set_shader_parameter("accent", accent)
+
+func _on_accent_b_value_changed(value: float) -> void:
+	accent.z = value
+	shader.set_shader_parameter("accent", accent)
 	
-	
-	if Input.is_action_pressed("left"):
-		current_pos = last_mouse_pos - get_global_mouse_position()
-		last_mouse_pos = get_global_mouse_position()
-		print(var_to_str(current_pos.x / 1152) + " " + var_to_str(current_pos.y / 648))
-		
-		shader.set_shader_parameter("offset_x", current_pos.x / 1152 * zoom)
-		shader.set_shader_parameter("offset_y", current_pos.y / 648 * zoom)
-		
-	#if Input.is_action_just_released("left"):
-		#print("action released")
-		#current_pos = get_global_mouse_position()
+
+func _on_intensity_value_changed(value: float) -> void:
+	shader.set_shader_parameter("accent_intensity", value)
